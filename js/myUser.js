947867cf7b1a9dd6804c55205172858966e7677f
f2e-noob <goalf2e@gmail.com>
2015-04-27 13:07:58 +0800

    ..

---
+++ js/myUser.js
@@ -0,0 +1,28 @@
+﻿function userController($scope) {
+	$scope.fName = '';
+	$scope.lName = '';
+	$scope.passw1 = '';
+	$scope.passw2 = '';
+	$scope.users = [
+	    {id: 1, fName:'Hege'  , lName:"Pege" },
+	    {id: 2, fName:'Kim'   , lName:"Pim" },
+	    {id: 3, fName:'Jack'  , lName:"Jones" },
+	];
+	$scope.edit=true;
+	$scope.erro=false;
+	$scope.incomplete=false;
+
+	$scope.editUser=function(id){
+		if( id='new' ){
+			$scope.edit=true;
+			$scope.incomplete = true;
+			$scope.fName = '';
+			$scope.lName = '';
+		}else{
+			$scope.edit= false;
+			$scope.fName = $scope.users[id-1].fName;
+       		$scope.lName = $scope.users[id-1].lName;        
+		}
+	}
+}
+
