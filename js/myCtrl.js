50ed7071c82d5d3a3db4b23db31296a19c5ab54b
f2e-noob <goalf2e@gmail.com>
2015-05-06 16:34:28 +0800

    ..

--- js/myCtrl.js
+++
@@ -1,8 +0,0 @@
-﻿app.controller("myCtrl", function($scope) {
-    $scope.persons =[
-    	{firstName:"xia",lastName:"qing"},
-    	{firstName:"huang",lastName:"ke"},
-    	{firstName:"zhang",lastName:"jun"},
-    ];
-});
- 
